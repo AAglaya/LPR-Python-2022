3ea1c5cbdc9ad12e5ad0f5f303abfe243bff1dee
AAglaya <59313041+AAglaya@users.noreply.github.com>
2022-09-30 10:02:18 +0300

    Add files via upload

--- classwork04/numpy_10.py
+++ classwork04/numpy_10.py
@@ -15,22 +15,22 @@ def x_pow(x, n):
         return np.dot(x_pow(x, n-1), x)
 
 
-k = int(input()) #степень, в которую надо возвести матрицу
-n = int(input()) #размер матрицы
-#x = np.random.randint(100, size=(n, n))      #<--- задать матрицу рандомно
-x = np.array([[1, 2], [3, 4]])
-x_i = np.eye(n)
-shape = (n, n)
-e = np.zeros(shape)
+def exp(k, n = 2, x = np.array([[1, 2], [3, 4]])):
+    x_i = np.eye(n)
+    shape = (n, n)
+    e = np.zeros(shape)
 
-for i in range(0, k+1):
-    if i == 0:
-        e += x_i
-    else:
-        temp = x_pow(x, i)
-        temp = temp / factorial(i)
-        e += temp
-print(e)
+    for i in range(0, k+1):
+        if i == 0:
+            e += x_i
+        else:
+            temp = x_pow(x, i)
+            temp = temp / factorial(i)
+            e += temp
+    return e
+
+
+print(exp(3))
 
 
 
